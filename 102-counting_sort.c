afc6064a6e3870e47aa1c24655b130519ee40952
AnnKamau <3annfaithk@gmail.com>
2023-11-19 11:03:07 -0800

    102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -4,14 +4,14 @@
  * get_max - Get the maximum value in an array of integers
  * @array: An array of integers
  * @size: The size of array
- * Return: The maximum integer in the array.
+ * Return: The maximum integer in the array
 
  */
 int get_max(int *array, int size)
 {
 	int maximum, h;
 
-	for (maximum = array[0], h = 1; h < size; h++)
+	for (maximum = array[0], h = 1; i < size; h++)
 	{
 		if (array[h] > maximum)
 			maximum = array[h];
@@ -23,12 +23,12 @@ int get_max(int *array, int size)
 /**
  * counting_sort - Sort an array of integers in ascending order
  * @array: An array of integers
- * @size: The size of the array
-
+ * @size: The size of array
+ 
  */
 void counting_sort(int *array, size_t size)
 {
-	int *add, *sorted, maximum, i;
+	int *count, *sorted, max, i;
 
 	if (array == NULL || size < 2)
 		return;
@@ -36,32 +36,32 @@ void counting_sort(int *array, size_t size)
 	sorted = malloc(sizeof(int) * size);
 	if (sorted == NULL)
 		return;
-	maximum = get_max(array, size);
-	add = malloc(sizeof(int) * (maximum + 1));
-	if (add == NULL)
+	max = get_max(array, size);
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
 	{
 		free(sorted);
 		return;
 	}
 
-	for (i = 0; i < (maximum + 1); i++)
-		add[i] = 0;
+	for (i = 0; i < (max + 1); i++)
+		count[i] = 0;
 	for (i = 0; i < (int)size; i++)
-		add[array[i]] += 1;
-	for (i = 0; i < (maximum + 1); i++)
-		add[i] += add[i - 1];
-	print_array(add, maximum + 1);
+		count[array[i]] += 1;
+	for (i = 0; i < (max + 1); i++)
+		count[i] += count[i - 1];
+	print_array(count, max + 1);
 
 	for (i = 0; i < (int)size; i++)
 	{
-		sorted[add[array[i]] - 1] = array[i];
-		cadd[array[i]] -= 1;
+		sorted[count[array[i]] - 1] = array[i];
+		count[array[i]] -= 1;
 	}
 
 	for (i = 0; i < (int)size; i++)
 		array[i] = sorted[i];
 
 	free(sorted);
-	free(add);
+	free(count);
 }
 
