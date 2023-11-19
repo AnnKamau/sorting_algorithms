041344b4778476ad3594a3c44c4d990c3832e952
AnnKamau <3annfaithk@gmail.com>
2023-11-19 09:49:40 -0800

    0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swap two integers in an array
+ * @a: The first integer
+ * @b: The second integer
+ */
+void swap_ints(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - Sort array of integers in ascending order
+ * @array: Array of integers to sort
+ * @size: size of the array
+
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t j, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (j = 0; j < len - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap_ints(array + j, array + j + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
+
